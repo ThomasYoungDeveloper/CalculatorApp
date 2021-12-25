fa236ed46717c8a0a6d47d84e170e019b07705e4
Thomas Young <Thomas.young228@gmail.com>
2021-12-25 14:42:10 -0500

    init commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+
+        if (tvInput.text.contains("1")) {
+            tvInput.text = "Haha"
+            }
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint (view: View) {
+        if(lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+
+        }
+    }
+
+    fun onOperator(view:View){
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+
+        }
+    }
+
+    private fun isOperatorAdded (value: String) : Boolean {
+        return if (value.startsWith("-")){
+            false
+        } else {
+            value.contains(("/")) || value.contains(("*")) || value.contains(("+")) || value.contains(("-"))
+        }
+
+    }
+}
