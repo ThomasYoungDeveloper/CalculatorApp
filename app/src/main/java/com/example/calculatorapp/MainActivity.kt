bf06d9b9fde3d04d8eb9e152638a13a78ff08d52
Thomas Young <Thomas.young228@gmail.com>
2021-12-26 20:33:27 -0500

    - added operation logic

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,16 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    var lastNumeric : Boolean = false
-    var lastDot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     fun onDigit(view: View) {
         tvInput.append((view as Button).text)
         lastNumeric = true
 
-        if (tvInput.text.contains("1")) {
-            tvInput.text = "Haha"
-            }
+
     }
 
     fun onClear(view: View) {
@@ -33,16 +32,92 @@ class MainActivity : AppCompatActivity() {
         lastDot = false
     }
 
-    fun onDecimalPoint (view: View) {
-        if(lastNumeric && !lastDot) {
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
             tvInput.append(".")
             lastNumeric = false
             lastDot = true
-
         }
     }
 
-    fun onOperator(view:View){
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var prefix = ""
+            var tvValue = tvInput.text.toString()
+
+            try {
+                if (tvValue.startsWith(("-"))) {
+                    tvValue = tvValue.substring(1)
+                }
+
+                // Subtraction
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+                // Addition
+                else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                // Multiplication
+                else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+                // Division
+                else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String {
+        var value = result
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+
+        }
+        return value
+    }
+
+    fun onOperator(view: View) {
         if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
             tvInput.append((view as Button).text)
             lastNumeric = false
@@ -51,11 +126,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun isOperatorAdded (value: String) : Boolean {
-        return if (value.startsWith("-")){
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
             false
         } else {
-            value.contains(("/")) || value.contains(("*")) || value.contains(("+")) || value.contains(("-"))
+            value.contains(("/")) || value.contains(("*")) || value.contains(("+")) || value.contains(
+                ("-")
+            )
         }
 
     }
